ac8182aca7a79bccc0fecd1f9116f441cb3a5c46
sevki <s@sevki.io>
2024-09-06 15:48:29 +0100

    fix test bump version

--- src/e2e_tests.rs
+++ src/e2e_tests.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
-
 #[test]
 fn debug() {
     let t = trybuild::TestCases::new();
+
     let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let path = std::path::PathBuf::from(cargo_manifest_dir);
     let path = path.join("integration/src/main.rs");
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,9 @@ pub mod oklog;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod okasync;
 
+mod e2e_tests;
 #[cfg(feature = "unstable")]
 pub mod okpanic;
-mod e2e_tests;
-
-extern crate fern;
 
 use log as rustlog;
 
@@ -16,35 +14,37 @@ pub mod prelude {
     pub use crate::okasync::*;
 
     pub use crate::oklog::setup_logging;
-    
-    #[cfg(feature = "macros")]    
-    pub use super::main;
+
     #[cfg(feature = "macros")]
     pub use super::log;
+    #[cfg(feature = "macros")]
+    pub use super::main;
 
     // re-export the slog macros
-    pub use fern::*;
     pub use crate::rustlog::debug;
     pub use crate::rustlog::error;
     pub use crate::rustlog::info;
     pub use crate::rustlog::trace;
     pub use crate::rustlog::warn;
+    pub use fern::*;
 
+    #[cfg(feature = "macros")]
+    #[cfg(feature = "unstable")]
+    pub use crate::okpanic::panic_hook;
     pub use crate::rustlog::LevelFilter;
     #[cfg(feature = "macros")]
     pub use std::panic::set_hook;
-    #[cfg(feature = "macros")]    
-    #[cfg(feature = "unstable")]
-    pub use crate::okpanic::panic_hook;
+
+    #[cfg(feature = "argh")]
+    pub use argh::*;
 }
 
+#[cfg(feature = "macros")]
+pub use ok_macros::log;
 #[cfg(feature = "macros")]
 pub use ok_macros::main;
 #[cfg(feature = "macros")]
 pub use ok_macros::test;
-#[cfg(feature = "macros")]
-pub use ok_macros::log;
-
 
 #[cfg(feature = "macros")]
 #[cfg(feature = "unstable")]
